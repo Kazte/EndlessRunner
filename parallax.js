80b410539d8dfc6b64b2780675a56d3f09c7e1a8
Lautaro <laujuvi@hotmail.com>
2019-07-08 14:43:41 -0300

    Paralax fixed

--- parallax.js
+++ parallax.js
@@ -25,7 +25,7 @@ class Parallax {
                 listBG.shift()
             }
             if ((listBG[i].x - camara.x) / par  + listBG[0].w < 800 && this.canGenerateBg) {
-                listBG.push(new BG((listBG[0].x - camara.x + listBG[0].w) * par + camara.x, 0, 1600, 995))
+                listBG.push(new BG(listBG[0].x - camara.x + listBG[0].w * par + camara.x, 0, 1600, 995))
                 this.canGenerateBg = false
             }
         }
